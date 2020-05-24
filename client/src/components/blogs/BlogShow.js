74dc76bed6f777135338375263d6ed4d641da567
Sharik <ssattera@gmail.com>
2020-05-24 18:02:42 +0530

    travis check

--- client/src/components/blogs/BlogShow.js
+++ client/src/components/blogs/BlogShow.js
@@ -6,6 +6,15 @@ class BlogShow extends Component {
   componentDidMount() {
     this.props.fetchBlog(this.props.match.params._id);
   }
+  
+
+  renderImage(){
+    if (this.props.blog.imageUrl){
+
+      return <img src = {'https://stark-blog-bucket.s3.ap-south-1.amazonaws.com/' + this.props.blog.imageUrl} />;
+    }
+
+  }
 
   render() {
     if (!this.props.blog) {
@@ -18,6 +27,7 @@ class BlogShow extends Component {
       <div>
         <h3>{title}</h3>
         <p>{content}</p>
+        {this.renderImage()}
       </div>
     );
   }
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
+const cleanCache = require('../middlewares/cleanCache');
+
 
 const Blog = mongoose.model('Blog');
 
@@ -13,16 +15,20 @@ module.exports = app => {
     res.send(blog);
   });
 
-  app.get('/api/blogs', requireLogin, async (req, res) => {
-    const blogs = await Blog.find({ _user: req.user.id });
-
+  app.get('/api/blogs', requireLogin,cleanCache, async (req, res) => {
+    
+    const blogs = await Blog.find({ _user: req.user.id}).cache({ key: req.user.id });
+    
     res.send(blogs);
+
+
   });
 
   app.post('/api/blogs', requireLogin, async (req, res) => {
-    const { title, content } = req.body;
+    const { title, content , imageUrl } = req.body;
 
     const blog = new Blog({
+      imageUrl,
       title,
       content,
       _user: req.user.id
@@ -31,8 +37,12 @@ module.exports = app => {
     try {
       await blog.save();
       res.send(blog);
+
     } catch (err) {
       res.send(400, err);
     }
+
+    clearHash(req.user.id);
+
   });
 };
